043cc6e8f31e845f8594f9e9890374ab50798253
SamarthGangrade22 <samarthgangrade@gmail.com>
2025-04-15 15:07:58 +0530

    Updates

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-module.exports = {
-    MONGODB_URI: "mongodb+srv://samarthgangrade:UqjDZXiLq1KfmTs4@cluster0.xfcollt.mongodb.net/server",
-    JWT_SECRET: "UqjDZXiLq1KfmTs4",
-  };
+// module.exports = {
+//     MONGODB_URI: "mongodb+srv://samarthgangrade:UqjDZXi@cluster0.xfcollt.mongodb.net/server",
+//     JWT_SECRET: "UqjDZXi",
+//   };
   
